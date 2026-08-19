9e61f46a7182acabdf9ccac87cb1fd1c6411ce1b
tarahi <fatahbibak@gmail.com>
2026-08-19 12:43:58 +0330

    Fix: Update configs for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import node from '@astrojs/node';
-import keystatic from '@keystatic/astro';
+   import { defineConfig } from 'astro/config';
+   import keystatic from '@keystatic/astro';
 
-// https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  integrations: [react(), markdoc(), keystatic()],
-});
+   export default defineConfig({
+     output: 'server',
+     integrations: [keystatic()],
+   });
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,27 +1,21 @@
-import { config, fields, collection } from '@keystatic/core';
+   import { config, fields, collection } from '@keystatic/core';
 
-export default config({
-  storage: {
-    kind: 'local',
-  },
-  collections: {
-    posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
-      schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({
-          label: 'Content',
-          options: {
-            image: {
-              directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
-            },
-          },
-        }),
-      },
-    }),
-  },
-});
+   export default config({
+     storage: {
+       kind: 'github',
+       repo: { 
+         owner: 'Fatah791', 
+         name: 'fata' 
+       },
+     },
+     collections: {
+       posts: collection({
+         label: 'Posts',
+         slugField: 'title',
+         schema: {
+           title: fields.slug({ name: { label: 'Title' } }),
+           content: fields.markdoc({ label: 'Content' }),
+         },
+       }),
+     },
+   });
